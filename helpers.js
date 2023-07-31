0013b0a1f09b235bf2467140f52535afc9634eb8
Abdul Rahman <lhlrahman@gmail.com>
2023-07-31 14:48:17 -0400

    add comments for readability

--- helpers.js
+++ helpers.js
@@ -1,3 +1,10 @@
+/**
+ * Retrieves a user object based on their email.
+ *
+ * @param {string} registerEmail - The email to search for in the users database.
+ * @param {object} users - The object representing the users database.
+ * @returns {object|null} - The user object if found, or null if not found.
+ */
 const getUserByEmail = function(registerEmail, users) {
   for (let userId in users) {
     if (users[userId].email === registerEmail) {
@@ -7,6 +14,12 @@ const getUserByEmail = function(registerEmail, users) {
   return null;
 };
 
+/**
+ * Generates a random alphanumeric string of length 6 that is not already in the urlDatabase.
+ *
+ * @param {object} urlDatabase - The object representing the urlDatabase.
+ * @returns {string} - The generated random string.
+ */
 const generateRandomString = function(urlDatabase) {
   const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
   let finalString = "";
@@ -27,6 +40,13 @@ const generateRandomString = function(urlDatabase) {
   return finalString;
 };
 
+/**
+ * Retrieves a user object based on their id.
+ *
+ * @param {string} id - The id to search for in the users database.
+ * @param {object} users - The object representing the users database.
+ * @returns {object|null} - The user object if found, or null if not found.
+ */
 const idLookup = function(id, users) {
   for (let userId in users) {
     if (users[userId].id === id) {
@@ -36,6 +56,13 @@ const idLookup = function(id, users) {
   return null;
 };
 
+/**
+ * Retrieves all URLs associated with a given cookieID from the urlDatabase.
+ *
+ * @param {string} cookieID - The id of the user's cookie.
+ * @param {object} urlDatabase - The object representing the urlDatabase.
+ * @returns {object} - An object containing the URLs associated with the given cookieID.
+ */
 const urlsForUser = function(cookieID, urlDatabase) {
   let urlsForUser = {};
   for (let url in urlDatabase) {
@@ -49,4 +76,4 @@ const urlsForUser = function(cookieID, urlDatabase) {
   return urlsForUser;
 };
 
-module.exports = { getUserByEmail, generateRandomString, idLookup, urlsForUser};
+module.exports = { getUserByEmail, generateRandomString, idLookup, urlsForUser };
